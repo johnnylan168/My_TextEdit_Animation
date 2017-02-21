856ca09771e3dedb2bb7fb2528e9e2c3b019f539
johnnylan168 <johnnylan168@gmail.com>
2017-02-21 22:31:27 +0800

    TextAnimation&EditAnimation

--- app/src/main/java/tw/lan/my_textedit_animation/MainActivity.java
+++ app/src/main/java/tw/lan/my_textedit_animation/MainActivity.java
@@ -2,12 +2,41 @@ package tw.lan.my_textedit_animation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView animText;
+    private EditText animEdit;
+    private Animation editAnimation, textAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        processView();
+        processController();
+    }
+
+    private void processView() {
+        animText = (TextView) findViewById(R.id.anim_text);
+        animEdit = (EditText) findViewById(R.id.anim_edit);
+    }
+
+    private void processController() {
+        editAnimation = AnimationUtils.loadAnimation(this, R.anim.edit_animation);
+        textAnimation = AnimationUtils.loadAnimation(this, R.anim.text_animation);
+    }
+
+    public void editAnim(View v) {
+        animEdit.startAnimation(editAnimation);
+    }
+
+    public void textAnim(View v) {
+        animText.startAnimation(textAnimation);
     }
 }
